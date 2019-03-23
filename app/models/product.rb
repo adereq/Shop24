f7e0dbe468ed602eccdd8f4210d651ffdfb8d33c
Adrian Kupiszewski <adrian.kupiszewski@intra.anixe.pl>
2019-03-23 19:02:22 +0100

    Adding routes

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,10 @@
 class Product < ApplicationRecord
   belongs_to :category
+
+  validates :name, presence: true
+  validates :descriptiom, presence: true
+  validates :price, numericality: {greater_than: 0.0}
+  validates :category, presence: true
+
+  mount_uploader :photo, ProductPhotoUploader
 end
